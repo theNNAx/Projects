e969085d94131986ce799fc39e70a0d3f704d222
theNNAx <npningpin@gmail.com>
2023-09-05 21:35:05 +0800

    Update swin_transformer.py
    9.5

--- Swin_transformer/swin_transformer.py
+++ Swin_transformer/swin_transformer.py
@@ -2,11 +2,85 @@ import torch
 import torch.nn as nn
 
 
-class SwinTransformer(nn.Module):
+def patch_partition(x, window_size: int):
+    pass
 
-    def __init__(self, in_chans, patch_size, window_size, embed_dim, depths, num_heads, num_classes):
+
+def patch_reverse(window, window_szie, H: int, W: int):
+    pass
+
+
+class PatchEmbed(nn.Module):
+    def __init__(self, patch_size, in_c, embed_dim, norm_layer):
         super().__init__()
 
+    def forward(self, x):
+        pass
+
+
+class PatchMerging(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+class Mlp(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+class WindowAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+class SwinTransformerBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+class BasicLayer(nn.Module):
+    def __init__(self, dim, depth, num_heads, ):
+        super().__init__()
+
+    def forward(self, x):
+        pass
+
+
+class SwinTransformer(nn.Module):
+    def __init__(self, in_chans, patch_size, window_size, embed_dim, depths, num_heads, num_classes,
+                 drop_rate, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.num_layers = len(depths)
+
+
+
+        self.patch_embed = PatchEmbed(patch_size=patch_size, in_c=in_chans, embed_dim=embed_dim,
+                                      norm_layer=norm_layer)
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        self.layers = nn.ModuleList()
+        for i_layer in range(self.num_layers):
+            layers = BasicLayer()
+
+
+
+            self.layers.append(layers)
+
+
+    def forward(self, x):
+        pass
 
 
 def swin_tiny_patch4_window7_224(num_classes: int = 1000):
